bb3ae8f91b6c0cc394696250ad5ba013e243127f
Eric Fiorim <eric.fiorim@gmail.com>
2022-02-10 22:44:51 -0300

    whell component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TimelineComponent } from './timeline/timeline.component';
+import { WheelComponent } from './wheel/wheel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TimelineComponent
+    TimelineComponent,
+    WheelComponent
   ],
   imports: [
     BrowserModule,
